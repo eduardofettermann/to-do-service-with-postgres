ba1df59cc8930c7e5d4f740d0f683064b55aae17
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-11-13 22:09:27 -0300

    first commit

---
+++ src/main/java/com/github/todoservicewithpostgres/ToDoServiceWithPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.github.todoservicewithpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoServiceWithPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoServiceWithPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/todoservicewithpostgres/ToDoServiceWithPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.todoservicewithpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoServiceWithPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
